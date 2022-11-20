f2bcab8f79257aa501d3b2b18ff24c9c3afe6807
Cassiopea2103 <wadess@ept.sn>
2022-11-20 14:49:51 +0000

    Adding middlewares

---
+++ backend/middlewares/errorHandler.js
@@ -0,0 +1,25 @@
+const { logEvents }= require('./logger')
+
+const errorHandler= (error ,request, response, next)=> {
+
+    const logMessage= `${error.name}: ${error.message}\t ${request.method}\t${request.url}
+    \t${request.headers.origin}`
+
+    logEvents(logMessage, 'errorLog.log')
+
+    console.log(error.stack)
+
+    const status= response.statusCode
+                ? response.statusCode 
+                : 500
+    
+    response.json(
+        {
+            "Message": error.message
+        }
+    )
+
+    next()
+}
+
+module.exports= { errorHandler }
--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,41 @@
+require('dotenv').config()
 const express= require('express')
 const app= express()
 
 const path= require('path')
 
-const { homePage }= require('./controllers/rootController')
+const cookieParser= require('cookie-parser')
+const cors= require('cors')
+const { corsOptions }= require('./config/corsOptions')
+const { logger }= require('./middlewares/logger')
+const { errorHandler }= require('./middlewares/errorHandler')
 
 const PORT= process.env.PORT || 3500
 
+// Middlewares: 
 // static middleware 
 app.use('/', express.static(path.join(__dirname, 'public')))
+
+//c cookies middleware: 
+app.use(cookieParser())  
+
+//json middleware:
+app.use(express.json())
+
+// cors middleware: 
+app.use(cors(corsOptions))
+
+// logger middleware: 
+app.use(logger)
+
 
 // views: 
 app.use('/', require('./routes/rootRoutes'))
 
 app.all('*', require('./routes/rootRoutes'))
+
+// error Handler middleware: 
+app.use(errorHandler)
 
 app.listen(
     PORT,
